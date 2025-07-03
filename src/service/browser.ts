b080d50ae5cab8b8a7b9ae7f0d040488e86ad56a
Mohammed Fadl <mohammadefathl@gmail.com>
2025-07-04 00:03:00 +0300

    refactor: remove unsed code

--- src/service/browser.ts
+++
@@ -1,9 +0,0 @@
-
-interface Browser {
-    addEventListener: typeof window.addEventListener;
-    navigator: typeof window.navigator;
-}
-export const browser: Browser = {
-    addEventListener: window.addEventListener.bind(window),
-    navigator: window.navigator
-};
